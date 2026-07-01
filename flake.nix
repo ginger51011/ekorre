a247a4595e37a77d8f875b9fb4377c97317ce794
Erogig <erogigabyte@gmail.com>
2026-07-01 19:28:04 +0200

    update nix flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "A prisma test project";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
@@ -10,16 +10,15 @@
       devShell = pkgs.mkShell {
         nativeBuildInputs = [ pkgs.bashInteractive ];
         buildInputs = with pkgs; [
-          prisma
+          nodePackages.prisma
           openssl
         ];
         shellHook = with pkgs; ''
-          export PRISMA_SCHEMA_ENGINE_BINARY="${prisma-engines}/bin/schema-engine"
+          export PRISMA_SCHEMA_ENGINE_BINARY="${prisma-engines}/bin/migration-engine" #export PRISMA_SCHEMA_ENGINE_BINARY="${prisma-engines}/bin/schema-engine"
           export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
           export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
-          export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
           export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
         '';
-      };
+      }; # export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
     });
 }
